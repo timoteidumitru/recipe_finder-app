328f6babe28581437e3f52d0b84b875767d1654b
Timotei Dumitru <dumitru.timy@gmail.com>
2020-09-27 15:04:02 +0100

    shorten the title of the recipe on the left section

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -6,6 +6,22 @@ export const clearInput = () => (elements.searchInput.value = "");
 
 export const clearResults = () => (elements.searchResultList.innerHTML = "");
 
+const limitRecTitle = (title, limit = 17) => {
+  const newTitle = [];
+  if (title.length > limit) {
+    title.split(" ").reduce((acc, cur) => {
+      if (acc + cur.length <= limit) {
+        newTitle.push(cur);
+      }
+      return acc + cur.length;
+    }, 0);
+
+    return `${newTitle.join(" ")} ...`;
+  }
+
+  return title;
+};
+
 const renderRecipe = (recipe) => {
   const markup = `
     <li>
@@ -14,7 +30,7 @@ const renderRecipe = (recipe) => {
               <img src="${recipe.image_url}" alt="${recipe.title}">
           </figure>
           <div class="results__data">
-              <h4 class="results__name">${recipe.title}</h4>
+              <h4 class="results__name">${limitRecTitle(recipe.title)}</h4>
               <p class="results__author">${recipe.publisher}</p>
           </div>
       </a>
