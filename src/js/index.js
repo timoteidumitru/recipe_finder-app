7699ff6ae61962d9ea4dd497a66160c7f44021e3
Timotei Dumitru <dumitru.timy@gmail.com>
2020-09-30 19:15:43 +0100

    basic setup of recipe controller

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 // Global app controller
 import Search from "./models/Search";
+import Recipe from "./models/Recipe";
 import * as searchView from "./views/searchView";
 import { elements, renderLoader, clearLoader } from "./views/base";
 
@@ -12,6 +13,9 @@ import { elements, renderLoader, clearLoader } from "./views/base";
 
 const state = {};
 
+/**
+ * SEARCH CONTROLLER
+ **/
 const controlSearch = async () => {
   // 1) Get the query from view
   const query = searchView.getInput(); //TODO
@@ -49,3 +53,11 @@ elements.searchResPages.addEventListener("click", (e) => {
     searchView.renderResults(state.search.result, goToPage);
   }
 });
+
+/**
+ * RECIPE CONTROLLER
+ **/
+
+const r = new Recipe(47746);
+r.getRecipe();
+console.log(r);
