063eca432ad9363ab6e06d07703ac8d3d9fff788
Timotei Dumitru <dumitru.timy@gmail.com>
2020-09-30 19:22:55 +0100

    add time and serving calculation for the recipes

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -11,11 +11,23 @@ export default class Recipe {
       );
       this.title = res.data.recipe.title;
       this.author = res.data.recipe.publisher;
-      this.img = res.data.recipe.img_url;
+      this.img = res.data.recipe.image_url;
       this.url = res.data.recipe.source_url;
       this.ingredients = res.data.recipe.ingredients;
     } catch (err) {
       console.log(err);
+      alert("something went wrong :(");
     }
+  }
+
+  calcTime() {
+    // Assuming that we need 15 min for each 3 ingredients
+    const numIng = this.ingredients.length;
+    const periods = Math.ceil(numIng / 3);
+    this.time = periods * 15;
+  }
+
+  calcServings() {
+    this.servings = 4;
   }
 }
