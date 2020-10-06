f089898f0dd6bac371cabad72a4ea1ad20a97407
Timotei Dumitru <dumitru.timy@gmail.com>
2020-10-06 19:20:13 +0100

    implement the model for shopping list items

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 // Global app controller
 import Search from "./models/Search";
 import Recipe from "./models/Recipe";
+import List from "./models/List";
 import * as searchView from "./views/searchView";
 import * as recipeView from "./views/recipeView";
 import { elements, renderLoader, clearLoader } from "./views/base";
@@ -116,3 +117,5 @@ elements.recipe.addEventListener("click", (e) => {
 
   console.log(state.recipe);
 });
+
+window.l = new List();
